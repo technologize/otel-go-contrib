f0d3224cd91121e6252f1b27c0391d2d9a9faad9
Bharat <96172898+bharat-gadde-dev@users.noreply.github.com>
2022-02-18 13:48:27 +0530

    Removed unnecessary Function

--- otelginmetrics/config.go
+++ otelginmetrics/config.go
@@ -1,11 +1,9 @@
 package otelginmetrics
 
 import (
-	"github.com/gin-gonic/gin"
 	"go.opentelemetry.io/otel/attribute"
 )
 
-type RequestURLLabelMappingFn func(c *gin.Context) string
 type config struct {
 	recordInFlight bool
 	recordSize     bool
